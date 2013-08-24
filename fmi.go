a81c5ea6a454348670cec6800c79953ca6dfa351
Vinhthuy Phan <vphan@memphis.edu>
2013-08-24 13:54:48 -0500

    Renamed FMindex to Index.  Placed SA into Index.

--- fmi.go
+++ fmi.go
@@ -1,5 +1,5 @@
 // Copyright 2013 Vinhthuy Phan
-// Package implements suffix array, uncompressed FM index
+// Package implements uncompressed FM index
 //
 package main
 
@@ -22,17 +22,16 @@ var Debug bool
 // Global variables: sequence (SEQ), suffix array (SA), BWT, FM index (C, OCC)
 //-----------------------------------------------------------------------------
 var SEQ []byte
-var SA []int
 var BWT []byte
 var Freq map[byte]int  // Frequency of each symbol
 
-type FMindex struct{
-	C map[byte]int  // count table
-	OCC map[byte][]int // occurence table
-	END_POS int // position of "$" in the text
-	SYMBOLS []int  // sorted symbols
-	EP map[byte]int // ending row/position of each symbol
-	SA []int
+type Index struct{
+	C map[byte]int  				// count table
+	OCC map[byte][]int 			// occurence table
+	END_POS int 					// position of "$" in the text
+	SYMBOLS []int  				// sorted symbols
+	EP map[byte]int 				// ending row/position of each symbol
+	SA []int 						// suffix array
 }
 //
 //-----------------------------------------------------------------------------
@@ -43,30 +42,29 @@ func (s BySuffix) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
 func (s BySuffix) Less(i, j int) bool { return (bytes.Compare(SEQ[s[i]:], SEQ[s[j]:]) == -1) }
 
 //-----------------------------------------------------------------------------
-func build_bwt(file string) int {
-	var end_pos int
+// BWT is saved into a separate file
+func (I *Index) BuildSA_BWT(file string) {
 	Freq = make(map[byte]int)
-	SA = make([]int, len(SEQ))
+	I.SA = make([]int, len(SEQ))
 	for i := 0; i < len(SEQ); i++ {
-		SA[i] = i
+		I.SA[i] = i
 		Freq[SEQ[i]]++
 	}
-	sort.Sort(BySuffix(SA))
+	sort.Sort(BySuffix(I.SA))
 
 	BWT = make([]byte, len(SEQ))
-	for i := 0; i < len(SA); i++ {
-		BWT[i] = SEQ[(len(SEQ)+SA[i]-1)%len(SEQ)]
+	for i := 0; i < len(I.SA); i++ {
+		BWT[i] = SEQ[(len(SEQ)+I.SA[i]-1)%len(SEQ)]
 		if BWT[i] == '$' {
-			end_pos = i
+			I.END_POS = i
 		}
 	}
 	ioutil.WriteFile(file, BWT, 0644)
 	fmt.Println("Save BWT to", file)
-	return end_pos
 }
 
 //-----------------------------------------------------------------------------
-func (I *FMindex) BuildIndex() {
+func (I *Index) BuildIndex() {
 	I.C = make(map[byte]int)
 	I.OCC = make(map[byte][]int)
 	I.EP = make(map[byte]int)
@@ -97,8 +95,8 @@ func (I *FMindex) BuildIndex() {
 }
 
 //-----------------------------------------------------------------------------
-func (I *FMindex) Save(file string) {
-	// Encode the struct FMindex y using Gob, then store it into "CompressFMindex.dat"
+func (I *Index) Save(file string) {
+	// Encode the struct Index y using Gob, then store it into "CompressIndex.dat"
 	var fout bytes.Buffer
 	enc := gob.NewEncoder(&fout)
 	err := enc.Encode(I)
@@ -110,51 +108,42 @@ func (I *FMindex) Save(file string) {
 }
 
 //-----------------------------------------------------------------------------
-func (I *FMindex) Search(pattern []byte, result chan int) {
+func (I *Index) Search(pattern []byte, result chan []int) {
 	var sp, ep, offset int
 	var ok bool
 
-	p := len(pattern)
-	c := pattern[p - 1]
-	sp, ok = I.C[byte(c)]
+	c := pattern[len(pattern) - 1]
+	sp, ok = I.C[c]
 	if ! ok {
-		// result <- make([]int, 0)
-		result <- 0
+		result <- make([]int, 0)
 		return
 	}
-	ep = I.EP[byte(c)]
+	ep = I.EP[c]
 	// if Debug { fmt.Println("pattern: ", string(pattern), "\n\t", string(c), sp, ep) }
-	for i:= p-1; i > 0 && sp <= ep; i-- {
-  		c = pattern[i - 1]
-  		offset, ok = I.C[byte(c)]
+	for i:= len(pattern)-2; sp <= ep && i >= 0; i-- {
+  		c = pattern[i]
+  		offset, ok = I.C[c]
   		if ok {
-			sp = offset + I.OCC[byte(c)][sp - 1]
-			ep = offset + I.OCC[byte(c)][ep] - 1
+			sp = offset + I.OCC[c][sp - 1]
+			ep = offset + I.OCC[c][ep] - 1
 		} else {
-			// result <- make([]int, 0)
-			result <- 0
+			result <- make([]int, 0)
 			return
 		}
   		// if Debug { fmt.Println("\t", string(c), sp, ep) }
 	}
-	if ep < sp {
-	  	// result <- make([]int, 0)
-	  	result <- 0
-	} else {
-		result <- ep-sp+1
-		// res := make([]int, ep-sp+1)
-		// for i:=sp; i<=ep; i++ {
-		// 	res[i-sp] = I.SA[i]
-		// }
-	 // 	result <- res
+	res := make([]int, ep-sp+1)
+	for i:=sp; i<=ep; i++ {
+		res[i-sp] = I.SA[i]
 	}
+ 	result <- res
 }
 
 //-----------------------------------------------------------------------------
 // return a length-l-substring of the text ending at position SA[r]-1
 // terminate if reaches beyond the first index.
 //-----------------------------------------------------------------------------
-func (I *FMindex) r_substr(r int, l int) []byte {
+func (I *Index) r_substr(r int, l int) []byte {
 	var s = make([]byte, l)
 	var i int
 	for i = l - 1; (i >= 0) && (r != I.END_POS); i-- {
@@ -168,7 +157,7 @@ func (I *FMindex) r_substr(r int, l int) []byte {
 }
 
 //-----------------------------------------------------------------------------
-func (I *FMindex) show() {
+func (I *Index) show() {
 	fmt.Printf(" %8s  OCC\n", "C")
 	for i := 0; i < len(I.SYMBOLS); i++ {
 		c := byte(I.SYMBOLS[i])
@@ -181,16 +170,15 @@ func (I *FMindex) show() {
 //-----------------------------------------------------------------------------
 // Build FM index given the file storing the text.
 // Usage:	idx := Build(text_file)
-func Build (file string) *FMindex {
-	I := new(FMindex)
+func Build (file string) *Index {
+	I := new(Index)
 
 	byte_array, err := ioutil.ReadFile(file)
 	if err != nil {
 		panic(err)
 	}
 	SEQ = append(byte_array, byte('$'))
-	I.END_POS = build_bwt(file+".bwt")
-	I.SA = SA
+	I.BuildSA_BWT(file+".bwt")
 	I.BuildIndex()
 	return I
 }
@@ -198,8 +186,8 @@ func Build (file string) *FMindex {
 //-----------------------------------------------------------------------------
 // Load FM index
 // Usage:  idx := Load(index_file)
-func Load (file string) *FMindex {
-	I := new(FMindex)
+func Load (file string) *Index {
+	I := new(Index)
 
 	finOCC,errOCC := os.Open(file)
 	decOCC := gob.NewDecoder(finOCC)
@@ -235,8 +223,9 @@ func main() {
 		// print_byte_array(BWT)
 		// fmt.Println(SA)
 	} else if *index_file!="" && *queries_file!="" {
-		result := make(chan int)
+		result := make(chan []int, 100000)
 		runtime.GOMAXPROCS(*workers)
+
 		idx := Load(*index_file)
 
 		f, err := os.Open(*queries_file)
@@ -253,7 +242,7 @@ func main() {
 			}
 		}
 		for i:=0; i<count; i++ {
-			fmt.Printf("Query %d %d\n", i, <-result)
+			fmt.Printf("%d\t%d\n", i+1, <- result)
 		}
 	}
 
