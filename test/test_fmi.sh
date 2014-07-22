54cfd146b2eb07b3d60dca70fce2e6fa67ad8c76
vtphan <vphan@memphis.edu>
2014-07-22 10:50:38 -0500

    ...

--- test/test_fmi.sh
+++ test/test_fmi.sh
@@ -1,6 +1,6 @@
 # requires correct output existed and stored in queries.out
-rm -r test.txt.index
+rm -r ref1.fasta.index
 rm /tmp/queries.out
-go run substr_search.go --build test.fasta
-go run substr_search.go -i test.fasta.index -q queries.txt > /tmp/queries.out
+go run substr_search.go --build ref1.fasta
+go run substr_search.go -i ref1.fasta.index -q q1.txt > /tmp/queries.out
 diff -y queries.out /tmp/queries.out
