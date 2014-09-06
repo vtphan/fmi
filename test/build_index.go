a162d3ff218b398c068d3ef18eb3146e041e798a
vtphan <vphan@memphis.edu>
2014-09-05 22:17:37 -0500

    update test build/load

--- test/build_index.go
+++ test/build_index.go
@@ -4,12 +4,28 @@ import (
    "github.com/vtphan/fmi"
    "os"
    "runtime"
+   "log"
 )
+
+
+var memstats = new(runtime.MemStats)
+
+func show_memstat(mesg string) {
+   runtime.ReadMemStats(memstats)
+   log.Printf("%s:\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", mesg,
+      float64(memstats.Alloc)/float64(1<<20),
+      float64(memstats.TotalAlloc)/float64(1<<20),
+      float64(memstats.Sys)/float64(1<<20),
+      float64(memstats.HeapAlloc)/float64(1<<20),
+      float64(memstats.HeapSys)/float64(1<<20))
+}
 
 //-----------------------------------------------------------------------------
 func main() {
    runtime.GOMAXPROCS(runtime.NumCPU())
+   show_memstat("before")
    idx := fmi.New(os.Args[1])
    idx.Save(os.Args[1])
    // idx.Show()
+   show_memstat("after")
 }
--- test/load_index.go
+++ test/load_index.go
@@ -5,14 +5,28 @@ import (
    "os"
    "fmt"
    "runtime"
+   "log"
 )
 
-var Debug bool
+
+var memstats = new(runtime.MemStats)
+
+func show_memstat(mesg string) {
+   runtime.ReadMemStats(memstats)
+   log.Printf("%s:\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", mesg,
+      float64(memstats.Alloc)/float64(1<<20),
+      float64(memstats.TotalAlloc)/float64(1<<20),
+      float64(memstats.Sys)/float64(1<<20),
+      float64(memstats.HeapAlloc)/float64(1<<20),
+      float64(memstats.HeapSys)/float64(1<<20))
+}
 
 //-----------------------------------------------------------------------------
 func main() {
    runtime.GOMAXPROCS(runtime.NumCPU())
+   show_memstat("before")
    idx := fmi.Load(os.Args[1])
    fmt.Println(idx.LEN)
    // idx.Show()
+   show_memstat("after")
 }
