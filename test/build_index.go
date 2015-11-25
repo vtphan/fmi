a5b187d8eb7c49ec2a6a9673f31c7d4d9b7db61d
Vinhthuy Phan <vphan@memphis.edu>
2015-11-25 15:28:20 -0600

    test files

--- test/build_index.go
+++ test/build_index.go
@@ -1,38 +1,37 @@
 package main
 
 import (
-   "github.com/vtphan/fmi"
-   "os"
-   "runtime"
-   "log"
-   // "fmt"
+	// "fmt"
+	"github.com/vtphan/fmi"
+	"log"
+	"os"
+	"runtime"
 )
-
 
 var memstats = new(runtime.MemStats)
 
 func show_memstat(mesg string) {
-   runtime.ReadMemStats(memstats)
-   log.Printf("%s:\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", mesg,
-      float64(memstats.Alloc)/float64(1<<10),
-      float64(memstats.TotalAlloc)/float64(1<<10),
-      float64(memstats.Sys)/float64(1<<10),
-      float64(memstats.HeapAlloc)/float64(1<<10),
-      float64(memstats.HeapSys)/float64(1<<10))
+	runtime.ReadMemStats(memstats)
+	log.Printf("%s:\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", mesg,
+		float64(memstats.Alloc)/float64(1<<10),
+		float64(memstats.TotalAlloc)/float64(1<<10),
+		float64(memstats.Sys)/float64(1<<10),
+		float64(memstats.HeapAlloc)/float64(1<<10),
+		float64(memstats.HeapSys)/float64(1<<10))
 }
 
 //-----------------------------------------------------------------------------
 func main() {
-   if len(os.Args) != 2 {
-      panic("Usage: go run build_index.go file.fasta")
-   }
-   runtime.GOMAXPROCS(runtime.NumCPU())
-   show_memstat("before")
-   idx := fmi.New(os.Args[1])
-   idx.Save(os.Args[1])
-   // for i:=0; i<len(idx.SA); i++ {
-   //    fmt.Println(i, idx.SA[i])
-   // }
-   idx.Show()
-   show_memstat("after")
+	if len(os.Args) != 2 {
+		panic("Usage: go run build_index.go file.fasta")
+	}
+	// runtime.GOMAXPROCS(runtime.NumCPU())
+	show_memstat("before")
+	idx := fmi.New(os.Args[1])
+	idx.Save(os.Args[1])
+	// for i := 0; i < len(idx.SA); i++ {
+	// 	fmt.Println(i, idx.SA[i])
+	// }
+	idx.Show()
+	show_memstat("after")
 }
--- test/load_index.go
+++ test/load_index.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-   "github.com/vtphan/fmi"
-   "os"
-   "fmt"
-   "runtime"
-   "log"
+	"fmt"
+	"github.com/vtphan/fmi"
+	"log"
+	"os"
+	"runtime"
 )
-
 
 var memstats = new(runtime.MemStats)
 
 func show_memstat(mesg string) {
-   runtime.ReadMemStats(memstats)
-   log.Printf("%s:\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", mesg,
-      float64(memstats.Alloc)/float64(1<<10),
-      float64(memstats.TotalAlloc)/float64(1<<10),
-      float64(memstats.Sys)/float64(1<<10),
-      float64(memstats.HeapAlloc)/float64(1<<10),
-      float64(memstats.HeapSys)/float64(1<<10))
+	runtime.ReadMemStats(memstats)
+	log.Printf("%s:\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", mesg,
+		float64(memstats.Alloc)/float64(1<<10),
+		float64(memstats.TotalAlloc)/float64(1<<10),
+		float64(memstats.Sys)/float64(1<<10),
+		float64(memstats.HeapAlloc)/float64(1<<10),
+		float64(memstats.HeapSys)/float64(1<<10))
 }
 
 //-----------------------------------------------------------------------------
 func main() {
-   runtime.GOMAXPROCS(runtime.NumCPU())
-   show_memstat("before")
-   idx := fmi.Load(os.Args[1])
-   fmt.Println(idx.LEN)
-   // idx.Show()
-   show_memstat("after")
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	show_memstat("before")
+	idx := fmi.Load(os.Args[1])
+	fmt.Println(idx.LEN)
+	idx.Show()
+	show_memstat("after")
 }
